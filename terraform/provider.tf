d8c2e4c7303dcacf5a58c11efa48142dcca32833
mrvan-design <taitomgamer2409@gmail.com>
2025-11-17 21:54:32 +0700

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,16 @@
-# =========================================
-# AWS provider trỏ tới LocalStack
-# =========================================
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.21.0"
+    }
+  }
+}
+
 provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
-
-  # Bỏ qua các check AWS thật
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
