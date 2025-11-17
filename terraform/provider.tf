436be60f2343f70db64c644b57850977f7b3a2a3
mrvan-design <taitomgamer2409@gmail.com>
2025-11-17 21:31:57 +0700

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,20 +1,14 @@
-# =========================================
-# AWS provider trỏ tới LocalStack
-# =========================================
 provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
-
-  # Bỏ qua các check AWS thật
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
   endpoints {
-    ec2 = "http://localhost:4566"
-    iam = "http://localhost:4566"
-    sts = "http://localhost:4566"
-    s3  = "http://localhost:4566"
+    ec2 = "http://host.docker.internal:4566"
+    vpc = "http://host.docker.internal:4566"
+    s3  = "http://host.docker.internal:4566"
   }
 }
