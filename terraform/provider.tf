be930aa695949e059a2dcc9d436ba0fe93ef439d
mrvan-design <taitomgamer2409@gmail.com>
2025-11-17 21:17:54 +0700

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,17 +2,25 @@ provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
-
-  # Bỏ qua các check không cần thiết
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
   endpoints {
-  ec2 = "http://127.0.0.1:4566"
-  iam = "http://127.0.0.1:4566"
-  sts = "http://127.0.0.1:4566"
-  s3  = "http://127.0.0.1:4566"
+    ec2 = "http://host.docker.internal:4566"
+    iam = "http://host.docker.internal:4566"
+    sts = "http://host.docker.internal:4566"
+    s3  = "http://host.docker.internal:4566"
+  }
+}
 
+resource "null_resource" "wait_for_localstack" {
+  provisioner "local-exec" {
+    command = <<EOT
+      until curl -s http://host.docker.internal:4566/_localstack/health | grep '"ec2": "running"'; do
+        echo "Waiting for LocalStack EC2..."
+        sleep 2
+      done
+    EOT
   }
 }
