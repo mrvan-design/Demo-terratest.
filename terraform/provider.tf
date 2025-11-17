774d95af81c8816870eac589c17d0ed901b79317
mrvan-design <taitomgamer2409@gmail.com>
2025-11-17 22:28:56 +0700

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.21.0"
-    }
-  }
-}
-
 provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
+
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
@@ -18,7 +10,6 @@ provider "aws" {
   endpoints {
     ec2 = "http://localhost:4566"
     iam = "http://localhost:4566"
-    sts = "http://localhost:4566"
-    s3  = "http://localhost:4566"
+    vpc = "http://localhost:4566"
   }
 }
